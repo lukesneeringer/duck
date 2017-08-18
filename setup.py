908ceca6417211b9fc13bb65b0a6c7623520a1f8
Luke Sneeringer <luke@sneeringer.com>
2017-08-17 20:58:01 -0700

    Fix the license.

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-# Copyright 2016 Jon Wayne Parrott
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2017 Luke Sneeringer
+# Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
 from setuptools import setup
 
@@ -38,7 +27,7 @@ setup(
 
         'Intended Audience :: Developers',
 
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
 
         'Topic :: Software Development :: Testing',
         'Environment :: Console',
